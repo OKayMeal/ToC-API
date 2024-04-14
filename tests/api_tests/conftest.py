b2129f69f48017154cf57a944743b0e832f28295
Kamil Kazimierczak <kaaaa765@gmail.com>
2024-04-14 17:10:02 +0200

    added test db clean up on teardown

--- tests/api_tests/conftest.py
+++ tests/api_tests/conftest.py
@@ -1,14 +1,21 @@
 import pytest
 import httpx
 from fastapi.testclient import TestClient
+from app.constants import TEST_API_KEY
 from app.main import app
 
 
 def pytest_addoption(parser):
+    """CLI options"""
     parser.addoption("--testenv", action="store", default="testClient")
 
-@pytest.fixture
+
+@pytest.fixture(scope="class")
 def client(request):
+    """
+    Choose env - dev or prod.
+    Default is testClient which performs the tests without running a server.
+    """
     test_env = request.config.getoption("--testenv")
     if test_env == "testClient":
         return TestClient(app)
@@ -18,3 +25,18 @@ def client(request):
         return httpx.Client(base_url='http://127.0.0.1:8000')
     else:
         raise ValueError("Invalid test environment")
+
+
+@pytest.fixture(scope="class")
+def setup_teardown(client: TestClient | httpx.Client):
+    """
+    Setup and Teardown
+    """
+    # setup
+
+    yield
+
+    # teardown
+    
+    client.delete("/clean-test", headers={ "X-API-KEY": TEST_API_KEY })
+    
